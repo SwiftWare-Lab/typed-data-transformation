837ee95f595d843ed1e2e2225e47b602db646ae0
Kazem Cheshmi <kazem.cheshmi@gmail.com>
2024-10-29 18:47:19 -0400

    removed the cmake-debug

--- cmake-build-debug/_deps/googlebenchmark-build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jamalids/Documents/compression-part4/new/big-data-compression/cmake-build-debug/_deps/googlebenchmark-src
-# Build directory: /home/jamalids/Documents/compression-part4/new/big-data-compression/cmake-build-debug/_deps/googlebenchmark-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("third_party/googletest/build")
-subdirs("test")
--- cmake-build-debug/_deps/googlebenchmark-build/third_party/googletest/googletest-paths.cmake
+++
@@ -1,2 +0,0 @@
-set(GOOGLETEST_SOURCE_DIR "/home/jamalids/Documents/compression-part4/new/big-data-compression/cmake-build-debug/_deps/googlebenchmark-build/third_party/googletest/src")
-set(GOOGLETEST_BINARY_DIR "/home/jamalids/Documents/compression-part4/new/big-data-compression/cmake-build-debug/_deps/googlebenchmark-build/third_party/googletest/build")
