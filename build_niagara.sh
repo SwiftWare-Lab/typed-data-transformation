9d220f233e76137863160baff5dc141259f8ebb8
Kazem <kazem.cheshmi@gmail.com>
2024-11-10 12:37:18 -0500

    added build_niagara

---
+++ build_niagara.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=64
+#SBATCH --export=ALL
+#SBATCH --job-name="compression"
+#SBATCH --mail-type=begin  # email me when the job starts
+#SBATCH --mail-type=end    # email me when the job finishes
+#SBATCH --mail-user=jamalids@mcmaster.ca
+#SBATCH --nodes=1
+#SBATCH --output="compression.%j.%N.out"
+#SBATCH --constraint=rome
+#SBATCH --mem=254000M
+#SBATCH -t 47:59:00
+
+#module load StdEnv/2023
+#module load gcc/13.3
+#module load cmake/3.27.7
+
+module load NiaEnv/.2022a
+module load gcc
+module load cmake
+
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+cd ..
+
