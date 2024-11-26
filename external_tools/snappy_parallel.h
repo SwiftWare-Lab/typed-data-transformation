84f18aef74dbb65de3a7d35af5c89dcbf4777b8b
samira <smrjamali@yahoo.com>
2024-11-25 19:51:32 -0500

    9

--- external_tools/snappy_parallel.h
+++ external_tools/snappy_parallel.h
@@ -1,11 +1,13 @@
-#ifndef LZ4_PARALLEL_H
-#define LZ4_PARALLEL_H
+//
+// Created by jamalids on 25/11/24.
+//
 
+#ifndef SNAPPY_PARALLEL_H
+#define SNAPPY_PARALLEL_H
 #include <vector>
 #include <iostream>
 #include <cstring>
-#include <lz4.h>
-#include <lz4hc.h>
+#include <snappy.h>
 #include "profiling_info.h"
 #include <omp.h>
 
@@ -59,71 +61,57 @@ void splitBytesIntoComponents(const std::vector<uint8_t>& byteArray,
   }
 }
 
-// Compress with LZ4
-size_t compressWithLZ4(const std::vector<uint8_t>& data, std::vector<uint8_t>& compressedData, int compressionLevel) {
-    int maxCompressedSize = LZ4_compressBound(data.size());
-    compressedData.resize(maxCompressedSize);
+// Compress with Snappy
+size_t compressWithSnappy(const std::vector<uint8_t>& data, std::vector<uint8_t>& compressedData) {
+  size_t compressedSize = snappy::MaxCompressedLength(data.size());
+  compressedData.resize(compressedSize);
 
-    int compressedSize;
-    if (compressionLevel > 0) {
-        // Use high-compression mode
-        compressedSize = LZ4_compress_HC(reinterpret_cast<const char*>(data.data()),
-                                         reinterpret_cast<char*>(compressedData.data()),
-                                         data.size(), maxCompressedSize, compressionLevel);
-    } else {
-        // Use fast compression mode
-        compressedSize = LZ4_compress_default(reinterpret_cast<const char*>(data.data()),
-                                              reinterpret_cast<char*>(compressedData.data()),
-                                              data.size(), maxCompressedSize);
-    }
+  snappy::RawCompress(reinterpret_cast<const char*>(data.data()), data.size(),
+                      reinterpret_cast<char*>(compressedData.data()), &compressedSize);
 
-    if (compressedSize <= 0) {
-        std::cerr << "LZ4 compression error." << std::endl;
-        return 0;
-    }
-
-    compressedData.resize(compressedSize);
-    return compressedSize;
+  compressedData.resize(compressedSize); // Resize to actual compressed size
+  return compressedSize;
 }
 
-// Decompress with LZ4
-size_t decompressWithLZ4(const std::vector<uint8_t>& compressedData, std::vector<uint8_t>& decompressedData, size_t originalSize) {
-    decompressedData.resize(originalSize);
-    int decompressedSize = LZ4_decompress_safe(reinterpret_cast<const char*>(compressedData.data()),
-                                               reinterpret_cast<char*>(decompressedData.data()),
-                                               compressedData.size(), originalSize);
-    if (decompressedSize < 0) {
-        std::cerr << "LZ4 decompression error." << std::endl;
-        return 0;
-    }
+// Decompress with Snappy
+size_t decompressWithSnappy(const std::vector<uint8_t>& compressedData, std::vector<uint8_t>& decompressedData, size_t originalSize) {
+  decompressedData.resize(originalSize);
 
-    return decompressedSize;
+  bool success = snappy::RawUncompress(reinterpret_cast<const char*>(compressedData.data()),
+                                       compressedData.size(),
+                                       reinterpret_cast<char*>(decompressedData.data()));
+  if (!success) {
+    std::cerr << "Snappy decompression error." << std::endl;
+    return 0;
+  }
+
+  return originalSize;
 }
+
+
 
 // Full compression without decomposition
-size_t lz4Compression(const std::vector<uint8_t>& data, ProfilingInfo &pi, std::vector<uint8_t>& compressedData) {
-    auto start = std::chrono::high_resolution_clock::now();
-    size_t compressedSize = compressWithLZ4(data, compressedData, 0); // Fast compression
-    auto end = std::chrono::high_resolution_clock::now();
+size_t snappyCompression(const std::vector<uint8_t>& data, ProfilingInfo &pi, std::vector<uint8_t>& compressedData) {
+  auto start = std::chrono::high_resolution_clock::now();
+  size_t compressedSize = compressWithSnappy(data, compressedData); // Fast compression
+  auto end = std::chrono::high_resolution_clock::now();
 
-    pi.type = "LZ4 Full Compression";
+  pi.type = "LZ4 Full Compression";
 
-    return compressedSize;
+  return compressedSize;
 }
-
 // Full decompression without decomposition
-void lz4Decompression(const std::vector<uint8_t>& compressedData, std::vector<uint8_t>& decompressedData, ProfilingInfo &pi, size_t originalSize) {
-    auto start = std::chrono::high_resolution_clock::now();
-    decompressWithLZ4(compressedData, decompressedData, originalSize);
-    auto end = std::chrono::high_resolution_clock::now();
+void snappyDecompression(const std::vector<uint8_t>& compressedData, std::vector<uint8_t>& decompressedData, ProfilingInfo &pi) {
+
+  decompressWithSnappy(compressedData, decompressedData, globalByteArray.size());
+
   // Verify decompressed data
   if (!verifyDataMatch(globalByteArray, decompressedData)) {
     std::cerr << "Error: Decompressed data doesn't match the original." << std::endl;
   }
-
 }
 // Sequential compression with decomposition that takes dynamic byte sizes as parameters
-size_t lz4DecomposedSequential(const std::vector<uint8_t>& data, ProfilingInfo &pi,
+size_t snappyDecomposedSequential(const std::vector<uint8_t>& data, ProfilingInfo &pi,
                                 std::vector<uint8_t>& compressedLeading,
                                 std::vector<uint8_t>& compressedContent,
                                 std::vector<uint8_t>& compressedTrailing,
@@ -132,17 +120,17 @@ size_t lz4DecomposedSequential(const std::vector<uint8_t>& data, ProfilingInfo &
     splitBytesIntoComponents(data, leading, content, trailing, leadingBytes, contentBytes, trailingBytes);
 
     auto start = std::chrono::high_resolution_clock::now();
-    size_t compressedSize_l = compressWithLZ4(leading, compressedLeading, 3);
+    size_t compressedSize_l = compressWithSnappy(leading, compressedLeading);
     auto end = std::chrono::high_resolution_clock::now();
     pi.leading_time = std::chrono::duration<double>(end - start).count();
 
     start = std::chrono::high_resolution_clock::now();
-    size_t compressedSize_c = compressWithLZ4(content, compressedContent, 3);
+    size_t compressedSize_c = compressWithSnappy(content, compressedContent);
     end = std::chrono::high_resolution_clock::now();
     pi.content_time = std::chrono::duration<double>(end - start).count();
 
     start = std::chrono::high_resolution_clock::now();
-    size_t compressedSize_t = compressWithLZ4(trailing, compressedTrailing, 3);
+    size_t compressedSize_t = compressWithSnappy(trailing, compressedTrailing);
     end = std::chrono::high_resolution_clock::now();
     pi.trailing_time = std::chrono::duration<double>(end - start).count();
 
@@ -151,7 +139,7 @@ size_t lz4DecomposedSequential(const std::vector<uint8_t>& data, ProfilingInfo &
 }
 
 // Sequential decompression with decomposition that  takes dynamic byte sizes as parameters
-void lz4DecomposedSequentialDecompression(const std::vector<uint8_t>& compressedLeading,
+void snappyDecomposedSequentialDecompression(const std::vector<uint8_t>& compressedLeading,
                                            const std::vector<uint8_t>& compressedContent,
                                            const std::vector<uint8_t>& compressedTrailing,
                                            ProfilingInfo &pi,
@@ -163,7 +151,7 @@ void lz4DecomposedSequentialDecompression(const std::vector<uint8_t>& compressed
 
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<uint8_t> tempLeading(floatCount * leadingBytes);
-    decompressWithLZ4(compressedLeading, tempLeading, floatCount * leadingBytes);
+    decompressWithSnappy(compressedLeading, tempLeading, floatCount * leadingBytes);
     for (size_t i = 0; i < floatCount; ++i) {
         for (size_t j = 0; j < leadingBytes; ++j) {
             reconstructedData[i * (leadingBytes + contentBytes + trailingBytes) + j] = tempLeading[i * leadingBytes + j];
@@ -174,7 +162,7 @@ void lz4DecomposedSequentialDecompression(const std::vector<uint8_t>& compressed
 
     start = std::chrono::high_resolution_clock::now();
     std::vector<uint8_t> tempContent(floatCount * contentBytes);
-    decompressWithLZ4(compressedContent, tempContent, floatCount * contentBytes);
+    decompressWithSnappy(compressedContent, tempContent, floatCount * contentBytes);
     for (size_t i = 0; i < floatCount; ++i) {
         for (size_t j = 0; j < contentBytes; ++j) {
             reconstructedData[i * (leadingBytes + contentBytes + trailingBytes) + leadingBytes + j] = tempContent[i * contentBytes + j];
@@ -185,7 +173,7 @@ void lz4DecomposedSequentialDecompression(const std::vector<uint8_t>& compressed
 
     start = std::chrono::high_resolution_clock::now();
     std::vector<uint8_t> tempTrailing(floatCount * trailingBytes);
-    decompressWithLZ4(compressedTrailing, tempTrailing, floatCount * trailingBytes);
+    decompressWithSnappy(compressedTrailing, tempTrailing, floatCount * trailingBytes);
     for (size_t i = 0; i < floatCount; ++i) {
         for (size_t j = 0; j < trailingBytes; ++j) {
             reconstructedData[i * (leadingBytes + contentBytes + trailingBytes) + leadingBytes + contentBytes + j] = tempTrailing[i * trailingBytes + j];
@@ -200,7 +188,7 @@ void lz4DecomposedSequentialDecompression(const std::vector<uint8_t>& compressed
     }
 }
 // Parallel compression with decomposition that takes dynamic byte sizes as parameters
-size_t lz4DecomposedParallel(const std::vector<uint8_t>& data, ProfilingInfo &pi,
+size_t snappyDecomposedParallel(const std::vector<uint8_t>& data, ProfilingInfo &pi,
                               std::vector<uint8_t>& compressedLeading,
                               std::vector<uint8_t>& compressedContent,
                               std::vector<uint8_t>& compressedTrailing,
@@ -216,7 +204,7 @@ size_t lz4DecomposedParallel(const std::vector<uint8_t>& data, ProfilingInfo &pi
         #pragma omp section
         {
             auto start = std::chrono::high_resolution_clock::now();
-            compressedSize_l = compressWithLZ4(leading, compressedLeading, 3);
+            compressedSize_l = compressWithSnappy(leading, compressedLeading);
             auto end = std::chrono::high_resolution_clock::now();
             pi.leading_time = std::chrono::duration<double>(end - start).count();
         }
@@ -224,7 +212,7 @@ size_t lz4DecomposedParallel(const std::vector<uint8_t>& data, ProfilingInfo &pi
         #pragma omp section
         {
             auto start = std::chrono::high_resolution_clock::now();
-            compressedSize_c = compressWithLZ4(content, compressedContent, 3);
+            compressedSize_c = compressWithSnappy(content, compressedContent);
             auto end = std::chrono::high_resolution_clock::now();
             pi.content_time = std::chrono::duration<double>(end - start).count();
         }
@@ -232,7 +220,7 @@ size_t lz4DecomposedParallel(const std::vector<uint8_t>& data, ProfilingInfo &pi
         #pragma omp section
         {
             auto start = std::chrono::high_resolution_clock::now();
-            compressedSize_t = compressWithLZ4(trailing, compressedTrailing, 3);
+            compressedSize_t = compressWithSnappy(trailing, compressedTrailing);
             auto end = std::chrono::high_resolution_clock::now();
             pi.trailing_time = std::chrono::duration<double>(end - start).count();
         }
@@ -245,7 +233,7 @@ size_t lz4DecomposedParallel(const std::vector<uint8_t>& data, ProfilingInfo &pi
 
 
 // Decompression function with dynamic byte segmentation and OpenMP parallelization
-void zstdDecomposedParallelDecompression(const std::vector<uint8_t>& compressedLeading,
+void sanppyDecomposedParallelDecompression(const std::vector<uint8_t>& compressedLeading,
                                          const std::vector<uint8_t>& compressedContent,
                                          const std::vector<uint8_t>& compressedTrailing,
                                          ProfilingInfo &pi,
@@ -261,7 +249,7 @@ void zstdDecomposedParallelDecompression(const std::vector<uint8_t>& compressedL
         {
             auto start = std::chrono::high_resolution_clock::now();
             std::vector<uint8_t> tempLeading(floatCount * leadingBytes);
-            decompressWithLZ4(compressedLeading, tempLeading, floatCount * leadingBytes);
+            decompressWithSnappy(compressedLeading, tempLeading, floatCount * leadingBytes);
             for (size_t i = 0; i < floatCount; ++i) {
                 for (size_t j = 0; j < leadingBytes; ++j) {
                     reconstructedData[i * (leadingBytes + contentBytes + trailingBytes) + j] = tempLeading[i * leadingBytes + j];
@@ -275,7 +263,7 @@ void zstdDecomposedParallelDecompression(const std::vector<uint8_t>& compressedL
         {
             auto start = std::chrono::high_resolution_clock::now();
             std::vector<uint8_t> tempContent(floatCount * contentBytes);
-            decompressWithLZ4(compressedContent, tempContent, floatCount * contentBytes);
+            decompressWithSnappy(compressedContent, tempContent, floatCount * contentBytes);
             for (size_t i = 0; i < floatCount; ++i) {
                 for (size_t j = 0; j < contentBytes; ++j) {
                     reconstructedData[i * (leadingBytes + contentBytes + trailingBytes) + leadingBytes + j] = tempContent[i * contentBytes + j];
@@ -289,7 +277,7 @@ void zstdDecomposedParallelDecompression(const std::vector<uint8_t>& compressedL
         {
             auto start = std::chrono::high_resolution_clock::now();
             std::vector<uint8_t> tempTrailing(floatCount * trailingBytes);
-            decompressWithLZ4(compressedTrailing, tempTrailing, floatCount * trailingBytes);
+            decompressWithSnappy(compressedTrailing, tempTrailing, floatCount * trailingBytes);
             for (size_t i = 0; i < floatCount; ++i) {
                 for (size_t j = 0; j < trailingBytes; ++j) {
                     reconstructedData[i * (leadingBytes + contentBytes + trailingBytes) + leadingBytes + contentBytes + j] = tempTrailing[i * trailingBytes + j];
@@ -306,4 +294,4 @@ void zstdDecomposedParallelDecompression(const std::vector<uint8_t>& compressedL
    }
 }
 
-#endif // LZ4_PARALLEL_H
+#endif //SNAPPY_PARALLEL_H
