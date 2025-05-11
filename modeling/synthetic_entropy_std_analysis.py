09111ee903ad05613cc453f84ce63a2caf60e7a2
Kazem <kazem.cheshmi@gmail.com>
2025-05-11 11:31:25 -0400

    WIP: KL

--- modeling/synthetic_entropy_std_analysis.py
+++ modeling/synthetic_entropy_std_analysis.py
@@ -1,9 +1,10 @@
 import numpy as np
+import pandas as pd
 import scipy.stats as stats
 
 import math
 from collections import Counter
-
+import time
 
 def generate_byte_stream(size, entropy):
     """
@@ -22,33 +23,23 @@ def generate_byte_stream(size, entropy):
     # Calculate the number of unique byte values needed
     num_symbols = int(2 ** entropy)
 
-    if entropy <= 8:
-        symbols = np.ones(num_symbols, dtype=np.uint8)
-    elif entropy <= 16:
-        symbols = np.ones(num_symbols, dtype=np.uint16)
-    elif entropy <= 32:
-        symbols = np.ones(num_symbols, dtype=np.uint32)
-    elif entropy <= 64:
-        symbols = np.ones(num_symbols, dtype=np.uint64)
-    else:
-        raise ValueError("Entropy too high for byte stream generation.")
-
     # Create probabilities for the symbols
     probabilities = np.ones(num_symbols) / num_symbols
 
     # Generate the byte stream
+    #np.random.seed(int(time.time()))
     symbols = np.random.choice(range(num_symbols), size=size, p=probabilities)
     # cast it as int8
-    # if entropy <= 8:
-    #     symbols = symbols.astype(np.uint8)
-    # elif entropy <= 16:
-    #     symbols = symbols.astype(np.uint16)
-    # elif entropy <= 32:
-    #     symbols = symbols.astype(np.uint32)
-    # elif entropy <= 64:
-    #     symbols = symbols.astype(np.uint64)
-    # else:
-    #     raise ValueError("Entropy too high for byte stream generation.")
+    if entropy <= 8:
+        symbols = symbols.astype(np.uint8)
+    elif entropy <= 16:
+        symbols = symbols.astype(np.uint16)
+    elif entropy <= 32:
+        symbols = symbols.astype(np.uint32)
+    elif entropy <= 64:
+        symbols = symbols.astype(np.uint64)
+    else:
+        raise ValueError("Entropy too high for byte stream generation.")
     # random shuffle the symbols
     #np.random.shuffle(symbols)
     return symbols
@@ -65,6 +56,24 @@ def compute_entropy(stream):
         entropy -= p * math.log2(p)
     return entropy
 
+
+def cross_entropy(dataset1, dataset2):
+    # Count occurrences of each element
+    counts1 = Counter(dataset1)
+    counts2 = Counter(dataset2)
+    total1 = len(dataset1)
+    total2 = len(dataset2)
+    # max size of two counts
+    max_size = max(len(counts1), len(counts2))
+    pk, qk = np.zeros(max_size), np.zeros(max_size)
+    for i in range(len(counts1.values())):
+        pk[i] = counts1[i] / total1
+    for i in range(len(counts2.values())):
+        qk[i] = counts2[i] / total2
+
+    # Calculate probabilities and cross entropy
+    cross_entropy = stats.entropy(pk, qk)
+    return cross_entropy
 
 def generate_float_stream(size, entropy_per_byte_array):
     num_components = len(entropy_per_byte_array)
@@ -115,54 +124,190 @@ def compress_with_zstd(data, level=3):
     comp_ratio = len(data.tobytes()) / len(compressed)
     return compressed, comp_ratio
 
+def plot_time_series(data, title):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    # Set the style of seaborn
+    sns.set(style="darkgrid")
+
+    # Create a time series plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(data, marker='o', linestyle='-', color='b')
+    plt.title(title)
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.grid(True)
+    plt.show()
 
 
-for i in range(1, 8):
-    float_stream, comp_array, comp_entropy_array = generate_float_stream(2*1024*1024, [4, 4, i, 8-i])
-    # print(f"Generated float stream of size {len(float_stream)} with entropy .")
-    # print(f"Entropy of the generated float stream: {compute_entropy(float_stream)} \n\n")
+def get_compression_ratio(components, merging_indices):
+    """
+    Calculate the compression ratio after merging components.
+
+    Parameters:
+    - components (list): List of components.
+    - merging_indices (list): Indices of components to merge.
+
+    Returns:
+    - float: Compression ratio after merging.
+    """
+    merged_component = np.concatenate([components[i] for i in merging_indices], axis=0)
+    compressed, comp_ratio = compress_with_zstd(merged_component)
+    entropy_merged = compute_entropy(merged_component)
     decomposed_size = 0
-    entropy_per_byte_array = []
-    for c in comp_array:
+    for c in components:
         decompressed, comp_ratio = compress_with_zstd(c)
         decomposed_size += len(decompressed)
-        entropy_per_byte_array.append(compute_entropy(c))
+    # flatten the components array
+    float_stream = np.concatenate(components, axis=0)
     decomp_cr = len(float_stream.tobytes()) / decomposed_size
-    orig_cop, original_cr = compress_with_zstd(float_stream)
-    entropy_bytes = compute_entropy(float_stream)
-    # cast it as int32
-    float_stream_i32 = float_stream.view(dtype=np.uint32)
-    #orig_cop32, original_cr32 = compress_with_zstd(float_stream_i32)
-    col_ord = np.concatenate(comp_array, axis=0)
-    transformed_data, transform_cr = compress_with_zstd(col_ord)
-    print(f"Entropy-orig: {entropy_bytes}  Entropy-32: {compute_entropy(float_stream_i32)}, array: {entropy_per_byte_array}, std: {np.std(entropy_per_byte_array)}")
-    print(f"CR decomp:  {decomp_cr}, order: {transform_cr}, original: {original_cr}\n\n")
+    return comp_ratio, decomp_cr, entropy_merged
+
+def check_mutual_information():
+    float_stream, comp_array, comp_entropy_array = generate_float_stream(
+        2 * 1024 * 1024, [2, 2, 4, 2])
+    float_stream2, comp_array2, comp_entropy_array2 = generate_float_stream(
+        2 * 1024 * 1024, [7, 7, 7, 7])
+    cross_table = np.zeros((len(comp_array), len(comp_array)))
+    comp_data, original_cr = compress_with_zstd(float_stream)
+    entropy_combined = compute_entropy(float_stream)
+    entropy_vals = np.zeros((len(comp_array)))
+    for i in range(len(comp_array)):
+        data = comp_array[i]
+        entropy_vals[i] = compute_entropy(data)
+    print(" Original compression ratio: ", original_cr)
+
+    print(f" ---ratio : {np.sum(entropy_vals)/entropy_combined} ---")
+    for i in range(len(comp_array)):
+        for j in range(i + 1, len(comp_array)):
+            data1 = comp_array[i]
+            data2 = comp_array[j]
+            e1 = compute_entropy(data1)
+            e2 = compute_entropy(data2)
+            # Calculate mutual information
+            mi = cross_entropy(data1, data2)
+            cross_table[i][j] = mi
+            cr, decomp_cr, e_merged = get_compression_ratio(comp_array, [i, j])
+            e_sum = e_merged
+            for k in range(len(comp_array)):
+                if k != i and k != j:
+                    e_sum += compute_entropy(comp_array[k])
+            print(f" ---ratio : {e_sum / entropy_combined} ---")
+            print(f"Mutual Information between component {i} and {j}: {mi}  -> {cr} vs {decomp_cr} vs {original_cr}")
 
 
 
-
-
-for i in range(1, 8):
-    float_stream, comp_array, comp_entropy_array = generate_float_stream(2*1024*1024, [7, 7, i, 8-i])
-    # print(f"Generated float stream of size {len(float_stream)} with entropy .")
-    # print(f"Entropy of the generated float stream: {compute_entropy(float_stream)} \n\n")
-    decomposed_size = 0
-    entropy_per_byte_array = []
-    for c in comp_array:
-        decompressed, comp_ratio = compress_with_zstd(c)
-        decomposed_size += len(decompressed)
-        entropy_per_byte_array.append(compute_entropy(c))
-    decomp_cr = len(float_stream.tobytes()) / decomposed_size
-    orig_cop, original_cr = compress_with_zstd(float_stream)
-    entropy_bytes = compute_entropy(float_stream)
-    # cast it as int32
-    float_stream_i32 = float_stream.view(dtype=np.uint32)
-    #orig_cop32, original_cr32 = compress_with_zstd(float_stream_i32)
-    print(f"Entropy-orig: {entropy_bytes}  Entropy-32: {compute_entropy(float_stream_i32)}, array: {entropy_per_byte_array}, std: {np.std(entropy_per_byte_array)}")
-    print(f"CR decomp:  {decomp_cr}, original: {original_cr}\n\n")
-
-
+check_mutual_information()
 exit(1)
+
+
+log_array = []
+
+# for j in range(1, 17):
+#     for i in range(1, 17):
+#         dict_log = {}
+#         float_stream, comp_array, comp_entropy_array = generate_float_stream(2*1024*1024, [17-i, 17-j])
+#
+#         # print(f"Generated float stream of size {len(float_stream)} with entropy .")
+#         # print(f"Entropy of the generated float stream: {compute_entropy(float_stream)} \n\n")
+#         decomposed_size = 0
+#         entropy_per_byte_array = []
+#         for c in comp_array:
+#             decompressed, comp_ratio = compress_with_zstd(c)
+#             decomposed_size += len(decompressed)
+#             entropy_per_byte_array.append(compute_entropy(c))
+#         decomp_cr = len(float_stream.tobytes()) / decomposed_size
+#         orig_cop, original_cr = compress_with_zstd(float_stream)
+#         entropy_bytes = compute_entropy(float_stream)
+#         # cast it as int32
+#         float_stream_i32 = float_stream.view(dtype=np.uint32)
+#         #orig_cop32, original_cr32 = compress_with_zstd(float_stream_i32)
+#         col_ord = np.concatenate(comp_array, axis=0)
+#         transformed_data, transform_cr = compress_with_zstd(col_ord)
+#         print(f"Entropy-orig: {entropy_bytes}  Entropy-32: {compute_entropy(float_stream_i32)}, array: {entropy_per_byte_array}, std: {np.std(entropy_per_byte_array)}")
+#         print(f"CR decomp:  {decomp_cr}, order: {transform_cr}, original: {original_cr}\n\n")
+#         dict_log["entropy"] = str(entropy_per_byte_array)
+#         dict_log["entropy bytes"] = str(entropy_bytes)
+#         dict_log["entropy int32"] = str(compute_entropy(float_stream_i32))
+#         dict_log["decomp cr"] = str(decomp_cr)
+#         dict_log["transform cr"] = str(transform_cr)
+#         dict_log["original cr"] = str(original_cr)
+#         dict_log["original size"] = str(len(float_stream))
+#         log_array.append(dict_log)
+#     # store as pandas df
+#     df = pd.DataFrame(log_array)
+#     df.to_csv(f"entropy_log.csv", index=False)
+#
+#
+# exit(1)
+
+
+# for l in range(1, 9):
+#     for k in range(1, 9):
+#         for j in range(1, 9):
+#             for i in range(1, 9):
+#                 dict_log = {}
+#                 float_stream, comp_array, comp_entropy_array = generate_float_stream(2*1024*1024, [i, j, k, l])
+#
+#                 # print(f"Generated float stream of size {len(float_stream)} with entropy .")
+#                 # print(f"Entropy of the generated float stream: {compute_entropy(float_stream)} \n\n")
+#                 decomposed_size = 0
+#                 entropy_per_byte_array = []
+#                 for c in comp_array:
+#                     decompressed, comp_ratio = compress_with_zstd(c)
+#                     decomposed_size += len(decompressed)
+#                     entropy_per_byte_array.append(compute_entropy(c))
+#                 decomp_cr = len(float_stream.tobytes()) / decomposed_size
+#                 orig_cop, original_cr = compress_with_zstd(float_stream)
+#                 entropy_bytes = compute_entropy(float_stream)
+#                 # cast it as int32
+#                 float_stream_i32 = float_stream.view(dtype=np.uint32)
+#                 #orig_cop32, original_cr32 = compress_with_zstd(float_stream_i32)
+#                 col_ord = np.concatenate(comp_array, axis=0)
+#                 transformed_data, transform_cr = compress_with_zstd(col_ord)
+#                 print(f"Entropy-orig: {entropy_bytes}  Entropy-32: {compute_entropy(float_stream_i32)}, array: {entropy_per_byte_array}, std: {np.std(entropy_per_byte_array)}")
+#                 print(f"CR decomp:  {decomp_cr}, order: {transform_cr}, original: {original_cr}\n\n")
+#                 dict_log["entropy"] = str(entropy_per_byte_array)
+#                 dict_log["entropy bytes"] = str(entropy_bytes)
+#                 dict_log["entropy int32"] = str(compute_entropy(float_stream_i32))
+#                 dict_log["decomp cr"] = str(decomp_cr)
+#                 dict_log["transform cr"] = str(transform_cr)
+#                 dict_log["original cr"] = str(original_cr)
+#                 dict_log["original size"] = str(len(float_stream))
+#                 log_array.append(dict_log)
+#             # store as pandas df
+#             df = pd.DataFrame(log_array)
+#             df.to_csv(f"entropy_log.csv", index=False)
+
+
+        #plot_time_series(float_stream_i32, f"randome data{i}")
+
+#
+#
+#
+#
+# for i in range(1, 8):
+#     float_stream, comp_array, comp_entropy_array = generate_float_stream(2*1024*1024, [7, 7, i, 8-i])
+#     # print(f"Generated float stream of size {len(float_stream)} with entropy .")
+#     # print(f"Entropy of the generated float stream: {compute_entropy(float_stream)} \n\n")
+#     decomposed_size = 0
+#     entropy_per_byte_array = []
+#     for c in comp_array:
+#         decompressed, comp_ratio = compress_with_zstd(c)
+#         decomposed_size += len(decompressed)
+#         entropy_per_byte_array.append(compute_entropy(c))
+#     decomp_cr = len(float_stream.tobytes()) / decomposed_size
+#     orig_cop, original_cr = compress_with_zstd(float_stream)
+#     entropy_bytes = compute_entropy(float_stream)
+#     # cast it as int32
+#     float_stream_i32 = float_stream.view(dtype=np.uint32)
+#     #orig_cop32, original_cr32 = compress_with_zstd(float_stream_i32)
+#     print(f"Entropy-orig: {entropy_bytes}  Entropy-32: {compute_entropy(float_stream_i32)}, array: {entropy_per_byte_array}, std: {np.std(entropy_per_byte_array)}")
+#     print(f"CR decomp:  {decomp_cr}, original: {original_cr}\n\n")
+
+# print(log_array)
+# exit(1)
 log_dict = {}
 for i in range(1, 31):
     print(f" -------------- Entropy: {i} --------------")
